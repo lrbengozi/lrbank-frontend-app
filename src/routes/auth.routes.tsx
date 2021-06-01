08cc867d7e87f8e65f128839285a1032272040b7
Luiz Rogerio <lrbengozi@gmail.com>
2021-06-01 20:07:02 -0300

    Change screen options on auth routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -7,12 +7,17 @@ import SignUp from '../pages/SignUp';
 const AuthStack = createStackNavigator();
 
 const AuthRoutes: React.FC = () => (
-  <AuthStack.Navigator
-    screenOptions={{
-      headerShown: false,
-    }}>
-    <AuthStack.Screen name="SignIn" component={SignIn} />
-    <AuthStack.Screen name="SignUp" component={SignUp} />
+  <AuthStack.Navigator>
+    <AuthStack.Screen
+      name="SignIn"
+      component={SignIn}
+      options={{headerShown: false}}
+    />
+    <AuthStack.Screen
+      name="SignUp"
+      component={SignUp}
+      options={{headerTintColor: '#e46400', headerTitle: 'Criar conta'}}
+    />
   </AuthStack.Navigator>
 );
 
