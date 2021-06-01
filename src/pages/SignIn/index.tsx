10eaf92be25d8f553e02fd9953de3f8c2764082f
Luiz Rogerio <lrbengozi@gmail.com>
2021-06-01 19:10:40 -0300

    Create global style

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,48 +3,7 @@ import {View, TouchableHighlight, StyleSheet, Text} from 'react-native';
 import {useAuth} from '../../contexts/auth';
 import {useNavigation} from '@react-navigation/native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-end',
-  },
-  textContainer: {
-    alignSelf: 'center',
-    marginBottom: 100,
-  },
-  textLogo: {
-    fontSize: 50,
-    color: '#e46400',
-    textAlign: 'center',
-    fontFamily: 'OdibeeSans-Regular',
-  },
-  buttons: {
-    padding: 10,
-  },
-  appButtonCreateAccount: {
-    marginTop: 10,
-    borderRadius: 10,
-    padding: 10,
-    backgroundColor: '#e46400',
-  },
-  appButtonCreateAccountText: {
-    fontSize: 16,
-    color: '#fff',
-    textAlign: 'center',
-  },
-  appButtonLogin: {
-    marginTop: 10,
-    borderRadius: 10,
-    borderWidth: 1,
-    padding: 10,
-    borderColor: '#e46400',
-  },
-  appButtonLoginText: {
-    fontSize: 16,
-    color: '#e46400',
-    textAlign: 'center',
-  },
-});
+import styles from '../../styles';
 
 function SignIn() {
   const {signIn} = useAuth();
