fa04ac68c9ea3d4bbc717c181406eb93793e8e78
Luiz Rogerio <lrbengozi@gmail.com>
2021-06-02 15:16:54 -0300

    Create modal view

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Modal, TouchableWithoutFeedback} from 'react-native';
 import {useAuth} from '../../contexts/auth';
 import {useNavigation} from '@react-navigation/native';
 
 import styles from '../../styles';
+import localStyle from './styles';
 import MainButton from '../../components/MainButton';
 import MainButtonOutline from '../../components/MainButtonOutline';
 import TitleText from '../../components/TitleText';
+import MainTextInput from '../../components/MainTextInput';
 
 function SignIn() {
+  const [modalVisible, setModalVisible] = React.useState(false);
+
   const {signIn} = useAuth();
   const navigation = useNavigation();
 
@@ -26,7 +30,31 @@ function SignIn() {
           caption="Criar Conta"
           onPress={() => navigation.navigate('SignUp')}
         />
-        <MainButtonOutline caption="Já tenho conta" onPress={() => {}} />
+        <MainButtonOutline
+          caption="Já tenho conta"
+          onPress={() => setModalVisible(!modalVisible)}
+        />
+        <TouchableWithoutFeedback
+          onPress={() => setModalVisible(!modalVisible)}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}>
+            <TouchableWithoutFeedback
+              onPress={() => setModalVisible(!modalVisible)}>
+              <View style={styles.footer}>
+                <View style={localStyle.modalView}>
+                  <MainTextInput
+                    placeholder="Numero da conta con dígito"
+                    keyboardType="numeric"
+                  />
+                  <MainTextInput placeholder="Senha" secureTextEntry={true} />
+                  <MainButton caption="Entrar" onPress={() => {}} />
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+          </Modal>
+        </TouchableWithoutFeedback>
       </View>
     </View>
   );
---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  modalView: {
+    backgroundColor: 'white',
+    borderTopEndRadius: 10,
+    borderTopStartRadius: 10,
+    padding: 5,
+  },
+});
+
+export default styles;
